f69dd1c7881053188d2fd3254a52cd1d5b25f966
umardev97 <umar.dev97@gmail.com>
2023-11-23 19:26:09 +0500

    webview

--- lib/widgets/WebView.dart
+++ lib/widgets/WebView.dart
@@ -14,7 +14,35 @@ class WebViewClass extends StatefulWidget {
 
 class _WebViewClassState extends State<WebViewClass> {
   WebViewController webViewController = WebViewController();
-
+setWebController(){
+  webViewController = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+          print(progress);
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('http://93.188.167.72/balance-admin/'));
+}
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setWebController();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +50,14 @@ class _WebViewClassState extends State<WebViewClass> {
         backgroundColor: AppColors.primaryColor,
         title:  Text(widget.title??""),
       ),
-      body: WebViewWidget(
-        controller: webViewController..loadRequest(Uri.parse(widget.url??"www.google.com"))
+      body:true?Center(
+        child: Text(
+          'In Progress.......',
+            style: TextStyle(color: AppColors.primaryColor,fontSize: 22)
+        ),
+      ): WebViewWidget(
+        controller: webViewController)
 
-      )
     );
   }
 }
